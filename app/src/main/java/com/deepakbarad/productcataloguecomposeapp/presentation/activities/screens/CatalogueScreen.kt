e6636b05deaed2a8cb20feab41e4ddd050669728
deepak <deepak.and.dev@gmail.com>
2023-09-05 18:34:45 +0530

    Load images from url using glide for compose

--- app/src/main/java/com/deepakbarad/productcataloguecomposeapp/presentation/activities/screens/CatalogueScreen.kt
+++ app/src/main/java/com/deepakbarad/productcataloguecomposeapp/presentation/activities/screens/CatalogueScreen.kt
@@ -1,9 +1,7 @@
 package com.deepakbarad.productcataloguecomposeapp.presentation.activities.screens
 
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -12,34 +10,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ClipboardManager
-import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.deepakbarad.productcataloguecomposeapp.domain.models.Product
 import com.deepakbarad.productcataloguecomposeapp.presentation.viewmodels.MainViewModel
-import java.util.concurrent.Flow
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalGlideComposeApi::class)
 @Composable
 fun CatalogueScreen(viewModel: MainViewModel) {
     Column(
@@ -56,6 +40,17 @@ fun CatalogueScreen(viewModel: MainViewModel) {
                 Row(
                     modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
                 ) {
+                    Column(modifier = Modifier
+                        .fillMaxWidth(0.30f)
+                        .background(Color.White)) {
+                        GlideImage(
+                            model = product.images[0],
+                            contentDescription = product.title,
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .fillMaxWidth(),
+                        )
+                    }
                     Column(
                         modifier = Modifier
                             .fillMaxWidth(0.70f)
@@ -71,10 +66,6 @@ fun CatalogueScreen(viewModel: MainViewModel) {
                             modifier = Modifier
                                 .padding(16.dp, 0.dp, 16.dp, 8.dp)
                         )
-                    }
-                    Column(modifier = Modifier.background(Color.White)) {
-
-
                     }
                 }
                 Divider()
